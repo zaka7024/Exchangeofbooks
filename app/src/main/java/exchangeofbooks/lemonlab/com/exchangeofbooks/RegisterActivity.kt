0a345c796b1ee9b2db8e958e97a30f2633f64f0c
zaka7024 <38314622+zaka7024@users.noreply.github.com>
2018-10-17 18:10:56 +0300

    Initial commit

---
+++ app/src/main/java/exchangeofbooks/lemonlab/com/exchangeofbooks/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package exchangeofbooks.lemonlab.com.exchangeofbooks
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
