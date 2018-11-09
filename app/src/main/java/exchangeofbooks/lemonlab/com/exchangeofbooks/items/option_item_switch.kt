eb85c0b6ad17289de84f502602cc6f472f4ba326
zaka7024 <zaka7024@gmail.com>
2018-11-09 15:39:12 +0200

    friends layout now support dark mode

--- app/src/main/java/exchangeofbooks/lemonlab/com/exchangeofbooks/items/option_item_switch.kt
+++ app/src/main/java/exchangeofbooks/lemonlab/com/exchangeofbooks/items/option_item_switch.kt
@@ -2,12 +2,14 @@ package exchangeofbooks.lemonlab.com.exchangeofbooks.items
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.CompoundButton
 import android.widget.Toast
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import exchangeofbooks.lemonlab.com.exchangeofbooks.R
+import exchangeofbooks.lemonlab.com.exchangeofbooks.SettingsActivity
 import kotlinx.android.synthetic.main.option_switch_row.view.*
 
 class option_item_switch(var type:String,var activity:Activity):Item<ViewHolder>() {
@@ -20,6 +22,12 @@ class option_item_switch(var type:String,var activity:Activity):Item<ViewHolder>
     override fun bind(viewHolder: ViewHolder, position: Int) {
 
         viewHolder.itemView.option_text_view.text = "Dark Mode"
+        var mode = shrPr.getString(activity.getString(exchangeofbooks.lemonlab.com.exchangeofbooks.R.string.mode),"li")
+
+        if(mode == "ni")
+            viewHolder.itemView.switch_btn_switch_item.isChecked = true
+        else
+            viewHolder.itemView.switch_btn_switch_item.isChecked = false
 
         viewHolder.itemView.switch_btn_switch_item.setOnCheckedChangeListener(object :CompoundButton.OnCheckedChangeListener{
             override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
@@ -28,6 +36,9 @@ class option_item_switch(var type:String,var activity:Activity):Item<ViewHolder>
                 }
                 else if(!isChecked)
                     setMode("li")
+                var intetn = Intent(activity,SettingsActivity::class.java)
+                activity.startActivity(intetn)
+                activity.finish()
             }
 
         })
